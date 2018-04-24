9a169f00e1054bac7d79513861599f8b051f20bf
anna770822 <anna770822@hotmail.com>
2018-04-24 10:59:18 +0800

    add user_id to post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180424024714) do
+ActiveRecord::Schema.define(version: 20180424025810) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20180424024714) do
     t.integer "view_counts"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
