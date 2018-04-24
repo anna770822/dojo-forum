c4df4b37141c8e2c650cdda82a9d36957f9cc085
anna770822 <anna770822@hotmail.com>
2018-04-24 22:17:46 +0800

    set admin category list

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,39 @@
 class Admin::CategoriesController < ApplicationController
   before_action :authenticate_admin
   def index
+    @categories = Category.all
+
+    if params[:id]
+      @category = Category.find(params[:id])
+    else
+      @category = Category.new
+    end
+  end
+
+  def create
+    @category = Category.new(category_param)
+    if @category.save
+      redirect_to admin_categories_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to admin_categories_path
+  end
+    
+  def update
+    @category = Category.find(params[:id])
+    @category.update(category_param)
+    redirect_to admin_categories_path
+  end
+
+  private
+
+  def category_param
+    params.require(:category).permit(:name)
   end
 end
