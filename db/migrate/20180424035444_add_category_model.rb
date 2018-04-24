61c0099d7c21ffbf7fbd65ec52cce443b772a665
anna770822 <anna770822@hotmail.com>
2018-04-24 11:58:45 +0800

    add category model

---
+++ db/migrate/20180424035444_add_category_model.rb
@@ -0,0 +1,4 @@
+class AddCategoryModel < ActiveRecord::Migration[5.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180424031049) do
+ActiveRecord::Schema.define(version: 20180424035614) do
+
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name"
+  end
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
