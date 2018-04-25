e190db0ef54c59d6ba95badb1a621a72bb2f4f4d
anna770822 <anna770822@hotmail.com>
2018-04-25 15:17:36 +0800

    add comment_count

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,16 +1,26 @@
 class CommentsController < ApplicationController
 
+
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
     @comment.user = current_user
     @comment.save
+    @post.comment_counts = @post.comments.count
     redirect_to post_path(@post)
   end
+
+  def destroy
+    @post = Post.find(params[:post_id])
+    @comment = Comment.where(post_id: @post).find(params[:comment_id])
+    @comment.destroy
+    redirect_to post_path(@post)
+  end
+
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id)
+    params.require(:comment).permit(:content, :post_id, :user_id, :image)
   end
 end
