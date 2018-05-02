1aac3fd110afdc8dd7364721f889f5d0b9effd7b
anna770822 <anna770822@hotmail.com>
2018-05-02 10:50:18 +0800

    add category links to root

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
   def index
     @q = Post.ransack(params[:q])
     @posts = @q.result(distinct: true).page(params[:page]).per(20)
+    @categories = Category.all
   end
 
   def new
