67169357af200184901ea347d720a515609f49bd
anna770822 <anna770822@hotmail.com>
2018-05-12 00:35:19 +0800

    api #create

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::PostsController < ApiController
+  before_action :authenticate_user!, except: :index
   before_action :set_category, only: [:index]
   before_action :set_post, only: [:show, :destroy, :update]
   def index
@@ -35,6 +36,22 @@ class Api::V1::PostsController < ApiController
       }
     end
   end
+  
+  def create
+
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      render json: {
+        message: "Post Created!",
+        result: @post
+      }
+    else
+      render json: {
+        errors: @post.errors
+      }
+    end
+  end
+
 
   private 
 
@@ -44,5 +61,9 @@ class Api::V1::PostsController < ApiController
 
   def set_post
     @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.permit(:title, :content, :image, :public, :authority, category_ids: [])
   end
 end
