eb007121a1dff92610a97f0a232d35ebf1295f0f
anna770822 <anna770822@hotmail.com>
2018-05-11 22:45:20 +0800

    API #index #show

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,48 @@
 class Api::V1::PostsController < ApiController
+  before_action :set_category, only: [:index]
+  before_action :set_post, only: [:show, :destroy, :update]
+  def index
+    if current_user
+      @q = Post.where(public: true).authorized_posts(current_user).ransack(params[:q])
+      @posts = @q.result(distinct: true).page(params[:page]).per(20)
+      respond_to do |format|
+        format.json{
+          render json: {:posts => @posts, 
+                        :categories => @categories
+          }
+        }
+      end
+    else
+      @q = Post.where(public: true).where(authority: "All").ransack(params[:q])
+      @posts = @q.result(distinct: true).page(params[:page]).per(20)
+      respond_to do |format|
+        format.json{
+          render json: {:posts => @posts, 
+                        :categories => @categories
+          }
+        }
+      end
+    end
+  end
+
+  def show
+    @comments = Comment.where(post_id: @post.id).page(params[:page]).per(20)
+    respond_to do |format|
+      format.json{
+            render json: {:post => @post, 
+                          :comments => @comments
+            }
+      }
+    end
+  end
+
+  private 
+
+  def set_category
+    @categories = Category.all
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -38,7 +38,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      resources :photos, only: [:index, :create, :show, :update, :destroy]
+      resources :posts, only: [:index, :create, :show, :update, :destroy]
     end
   end
 end
