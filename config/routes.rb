355902e7a8a77ebc93c9f33909a2781838e1d4d2
anna770822 <anna770822@hotmail.com>
2018-05-11 22:01:58 +0800

    add api routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -35,4 +35,10 @@ Rails.application.routes.draw do
     resources :users, only: [:index, :update]
     resources :categories
   end
+
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      resources :photos, only: [:index, :create, :show, :update, :destroy]
+    end
+  end
 end
